a38f54bc18bb4c226eabf3e7e7ba81567766bd7c
AlexanderVinarsky <vinarskydichtv@gmail.com>
2022-08-23 21:33:48 +0300

    Final

--- ConsoleBot/Program.cs
+++ ConsoleBot/Program.cs
@@ -19,24 +19,41 @@ namespace ConsoleApp
         public static List<string> l_answers = new List<string>();
         public static string token = "";
         public static VkApi vk = new VkApi();
+        public static long idOfConversation = 0;
         static void Main(string[] args)
         {
             string path1 = Environment.CurrentDirectory + "/questions.txt";
             string path2 = Environment.CurrentDirectory + "/answers.txt";
             string path3 = Environment.CurrentDirectory + "/APItoken.txt";
-            if (File.Exists(path1) && File.Exists(path2) && File.Exists(path3))
+            string path4 = Environment.CurrentDirectory + "/ConversationID.txt";
+            if (File.Exists(path1) && File.Exists(path2) && File.Exists(path3) && File.Exists(path4))
             {
                 using (StreamReader sr1 = new StreamReader(path1))
                 {
-                    while (!sr1.EndOfStream) l_questions.Add(sr1.ReadLine());
+
+                    while (!sr1.EndOfStream)
+                    {
+                        string temp = sr1.ReadLine();
+                        l_questions.Add(temp);
+                    }
+
                 }
                 using (StreamReader sr2 = new StreamReader(path2))
                 {
-                    while (!sr2.EndOfStream) l_answers.Add(sr2.ReadLine());
+                    while (!sr2.EndOfStream)
+                    {
+                        string temp = sr2.ReadLine();
+                        l_answers.Add(temp);
+                    }
+                    l_answers.Add(sr2.ReadLine());
                 }
-                using (StreamReader sr3 = new StreamReader(path2))
+                using (StreamReader sr3 = new StreamReader(path3))
                 {
                     token = sr3.ReadToEnd();
+                }
+                using (StreamReader sr4 = new StreamReader(path4))
+                {
+                    idOfConversation = long.Parse(sr4.ReadToEnd());
                 }
             }
             vk.Authorize(new ApiAuthParams
@@ -64,25 +81,16 @@ namespace ConsoleApp
                 code = minfo[1].ToString();
             else
                 code = minfo[0].ToString();
-            int a = 0;
-            foreach (var item in l_questions)
+
+            for (int i = 0; i < l_questions.Count; i++)
             {
-                a++;
-                Console.WriteLine(a + " " + item);
-                if (code.Contains(item))
+                if (code.Contains(l_questions[i]))
                 {
+                    SendMessage(l_answers[i], userid, null);
                     break;
                 }
             }
-            foreach (var item in l_answers)
-            {
-                a--;
-                if (a == 0)
-                {
-                    SendMessage(item, userid, null);
-                    break;
-                }
-            }
+
             return true;
         }
         public static void SendMessage(string message, long? userid, MessageKeyboard keyboard)
@@ -95,6 +103,9 @@ namespace ConsoleApp
                 Keyboard = keyboard
             });
         }
+
+
+        [Obsolete]
         public static object[] GetMessage()
         {
             string message = "";
@@ -103,29 +114,35 @@ namespace ConsoleApp
             long? conversationid = 0;
             var messages = vk.Messages.GetDialogs(new MessagesDialogsGetParams
             {
-                Count = 1,
+                Count = 100,
                 Unread = true
             });
             if (messages.Messages.Count != 0)
             {
-                if (messages.Messages[0].Body.ToString() != "" && messages.Messages[0].Body.ToString() != null)
-                    message = messages.Messages[0].Body.ToString();
-                else
-                    message = "";
+                if (messages.Messages[0].ChatId != null) if (messages.Messages[0].ChatId.Value == idOfConversation)
+                    {
+                        if (messages.Messages[0].Body.ToString() != "" && messages.Messages[0].Body.ToString() != null)
+                            message = messages.Messages[0].Body.ToString();
+                        else
+                            message = "";
 
-                if (messages.Messages[0].Payload != null)
-                    keyname = messages.Messages[0].Payload.ToString();
-                else
-                    keyname = "";
-                userid = messages.Messages[0].UserId.Value;
+                        if (messages.Messages[0].Payload != null)
+                            keyname = messages.Messages[0].Payload.ToString();
+                        else
+                            keyname = "";
+                        userid = messages.Messages[0].UserId.Value;
 
-                if (messages.Messages[0].ChatId != null) conversationid = messages.Messages[0].ChatId.Value;
+                        if (messages.Messages[0].ChatId != null) conversationid = messages.Messages[0].ChatId.Value;
 
 
 
-                object[] keys = new object[3] { message, keyname, userid };
-                if (conversationid > 0) vk.Messages.MarkAsRead((conversationid + 2000000000).ToString());
-                return keys;
+                        object[] keys = new object[3] { message, keyname, userid };
+                        if (conversationid > 0) vk.Messages.MarkAsRead((conversationid + 2000000000).ToString());
+                        return keys;
+                    }
+                    else
+                        return new object[] { null, null, null };
+                else return new object[] { null, null, null };
             }
             else
                 return new object[] { null, null, null };
